544aa33b1f95e72e854f5ed26a70dce39056161d
justinsnider <thesnide@Justins-Macintosh.local>
2016-10-20 11:46:14 -0700

    Masonry

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,9 @@ $('h1').lettering();
 $.backstretch('../img/bg.jpg');
 
 //Make Masonry Go
+$('article').masonry({
+    itemSelector: 'h1',
+    columnWidth: 0,
+    gutter: 10,
+    transitionDuration: '1s'
+})
